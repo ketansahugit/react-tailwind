fc6cfde3996c613be6ef51d8a11863222aaa14a3
Ketan Sahu <rorozoro9888@gmail.com>
2023-09-28 19:26:22 +0530

    project complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         <div onClick={handleNav} className='block md:hidden'>
           {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <div className={ nav ? 'fixed left-0 top-0 w-[60%] border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
           <ul className='uppercase p-4 border-gray-600'>
           <li className='p-4 border-b border-gray-600'>Home</li>
